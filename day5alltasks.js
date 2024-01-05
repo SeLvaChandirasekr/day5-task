5ef0b0ade022a48e45aed26f75b2f082b4caf9a3
SeLvaChandirasekr <154896256+SeLvaChandirasekr@users.noreply.github.com>
2024-01-05 12:08:53 +0530

    Add files via upload

---
+++ day5alltasks.js
@@ -0,0 +1,67 @@
+// using for loop
+// function getResume() {
+//     var resume = [{
+//         name :"Selvam",
+// 			lastName :"Chandirasekaran",
+// 			age:28,
+// 			qualification:"Msc",
+// 	        city:"Tirupattur",
+// 		    location:"Jolarpettai",
+// 		    contactNumber :9655842125
+//     }]
+
+//     for (i=0;i<resume.length;i++) {
+// console.log(resume[i])    }
+// }getResume() 
+
+// using for in
+
+// function getResume() {
+//     var resume = [{name :"Selvam",
+//                 lastName :"Chandirasekaran",
+//                 age:28,
+//                 qualification:"Msc",
+//                 city:"Tirupattur",
+//                 location:"Jolarpettai",
+//                 contactNumber :9655842125
+    
+//     }]
+//     for (let i in resume ) {
+//     console.log(resume[i])
+//     } 
+// }getResume()
+
+//using forEach loop
+// function getResume() {
+//     var resume = [{
+//     name :"Selvam",
+//                 lastName :"Chandirasekaran",
+//                 age:28,
+//                 qualification:"Msc",
+//                 city:"Tirupattur",
+//                 location:"Jolarpettai",
+//                 contactNumber :9655842125
+    
+//     }]
+//     resume.forEach (function(a) {
+//     console.log(a)
+//     });
+// }getResume()
+
+
+//for of loop
+
+// function getResume() {
+//     var resume = [{name :"Selvam",
+//                 lastName :"Chandirasekaran",
+//                 age:28,
+//                 qualification:"Msc",
+//                 city:"Tirupattur",
+//                 location:"Jolarpettai",
+//                 contactNumber :9655842125
+    
+//     }]
+//     for (var i of resume ) {
+//     console.log(i)
+//     }
+//     }getResume()
